2ca8ae45adad04c80d33c203c752f66f720aca0a
israel <thaelmacedo@gmail.com>
2023-07-19 20:08:23 -0300

    login

--- dietas/dietas_app/urls.py
+++ dietas/dietas_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index,processar_formulario
+from .views import index,processar_formulario, login, logout
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('index', index, name='index'),
     path('processar_formulario', processar_formulario, name='processar_formulario'),
+    path('', login , name='login'),
+    path('logout/', logout , name='logout'),
 ]
--- dietas/dietas_app/views.py
+++ dietas/dietas_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
     return render(request, 'index.html')
@@ -27,9 +29,27 @@ def processar_formulario(request):
         # Redirecione para outra página ou renderize um template de confirmação
         return render(request, 'confirmacao.html', context)
 
-    # Se não for uma requisição POST, retorne uma página vazia ou redirecione para outra página
+    a
     return render(request, 'vazio.html')
 
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index') 
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout(request):
+    logout(request)
+    return redirect('login')
 
 
 
