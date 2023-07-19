5bacfd37bef582f98df3fd21cc9f8c6bfbcf4e28
israel <thaelmacedo@gmail.com>
2023-07-19 20:16:05 -0300

    login funcionando

--- dietas/dietas_app/views.py
+++ dietas/dietas_app/views.py
@@ -40,9 +40,8 @@ def login(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('index') 
+            
+            return redirect('index') 
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
