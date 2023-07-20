a7b475588953716ec5c7364769012606338e9e26
William Szalai <williamszalai@gmail.com>
2023-07-19 21:13:14 -0300

    css funcionando

--- dietas/dietas/settings.py
+++ dietas/dietas/settings.py
@@ -121,7 +121,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'dietas_app/static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
