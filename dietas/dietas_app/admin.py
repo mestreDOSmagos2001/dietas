370732c44dbaaae0a211e29646865e75cc482390
Macedorael <raelmacedodasilva@gmail.com>
2023-07-24 14:34:49 -0300

    vamos

--- dietas/dietas_app/admin.py
+++ dietas/dietas_app/admin.py
@@ -3,7 +3,7 @@ from .models import Paciente
 
 
 class PacienteAdmin(admin.ModelAdmin):
-    list_display = ('nome','telefone','email')
+    list_display = ('nome','telefone','email','criado','modificado','ativo')
 
 
 admin.site.register(Paciente, PacienteAdmin)
--- dietas/dietas_app/forms.py
+++ dietas/dietas_app/forms.py
@@ -4,4 +4,4 @@ from .models import Paciente
 class PacienteForm(forms.ModelForm):
     class Meta:
         model = Paciente
-        fields = ['nome', 'telefone', 'email']
+        fields = ['nome', 'telefone', 'email',]
